303a633c4a2c00573db659aaccccb5e7deacc0f8
keisuke <keisuke@cd1ffd91-e609-42d8-b181-33580f4ca86f>
2011-06-18 06:00:31 +0000

    Improved.
    git-svn-id: file:///srv/subversion/icfp2011@23 cd1ffd91-e609-42d8-b181-33580f4ca86f

--- players/ltg_help_attack.cc
+++ players/ltg_help_attack.cc
@@ -54,20 +54,6 @@ void _(T lhs, U rhs) {
   Opp();
 }
 
-void ApplyAtZero(int i) {
-               // i: F
-  _(K, i);     // i: K(F)
-  _(S, i);     // i: S(K(F))
-  _(i, GET);   // i: S(K(F))(get)
-  _(i, ZERO);  // i: S(K(F))(get)(zero) -> F(get zero)
-}
-
-void DoubleN(int i, int n) {
-  for (int j = 0; j < n; ++j) {
-    _(DBL, i);
-  }
-}
-
 // Assumes that slot i has value zero.
 void ToN(int i, int n) {
   if (n != 0) {
@@ -79,34 +65,65 @@ void ToN(int i, int n) {
   }
 }
 
-void Succ(int i, int n) {
-  for (int j = 0; j < n; ++j) {
-    _(SUCC, i);
+// Assumes that slot i has valeu I.
+void IToN(int i, int n) {
+  _(i, ZERO);
+  ToN(i, n);
+}
+
+void Wrap(int i, Card c) {
+            // i: F
+  _(K, i);  // i: K(F)
+  _(S, i);  // i: S(K(F))
+  _(i, c);  // i: S(K(F))(c)
+}
+
+void WrapSuccN(int i, int n) {
+  if (n == 0) return;
+  WrapSuccN(i, n / 2);
+  Wrap(i, DBL);
+  if (n & 1) Wrap(i, SUCC);
+}
+
+// Calls a function at slot i with a value at slot j.
+void CallWithSlot(int i, int j) {
+  // i: F
+  Wrap(i, GET);  // i: S(K(F))(get)
+  //WrapSuccN(i, j);
+  for (int k = 0; k < j; ++k) {
+    Wrap(i, SUCC);  // i: S(K(S(K(F))(get)))(succ)
   }
+  _(i, ZERO);  // i: F(get(succ(succ(...(zero)...))))
+}
+
+// Calls a function at slot i with a value j
+void CallWithValue(int i, int j) {
+  // i: F
+  for (int k = 0; k < j; ++k) {
+    Wrap(i, SUCC);  // i: S(K(F))(succ)
+  }
+  _(i, ZERO);  // i: F(succ(succ(...(zero)...)))
 }
 
 void DoWork() {
   // Assumes 0: I
   // _(PUT, 0);
-  _(0, ZERO);  // 0: 0
-  ToN(0, 3);  // 0: 3
+  IToN(0, 3);  // 0: 3
   _(GET, 0);  // 0: 8192
 
   // Repeat help(0)(1)(8192) and help(1)(0)(8192) to increase life.
   for (int i = 0; i < 68; ++i) {
     // help(0)(1)(8192)
     // Assumes 0: 8192
-    _(1, ZERO);  // 1: 0
-    ToN(1, 4);  // 1: 4
+    IToN(1, 4);  // 1: 4
     _(GET, 1);  // 1: help(0)(1)
-    ApplyAtZero(1);  // help(0)(1)(8192) -> I
+    CallWithSlot(1, 0);  // help(0)(1)(8192) -> I
 
     // help(1)(0)(8192)
     // Assumes 0: 8192
-    _(1, ZERO);  // 1: 0
-    ToN(1, 5);  // 1: 5
+    IToN(1, 5);  // 1: 5
     _(GET, 1);  // 1: help(1)(0)
-    ApplyAtZero(1);  // help(1)(0)(8192) -> I
+    CallWithSlot(1, 0);  // help(1)(0)(8192) -> I
   }
 
   _(PUT, 0);  // 0: I
@@ -121,12 +138,8 @@ void DoWork() {
     _(ATTACK, 1);  // 1: attack(0)
     _(0, ZERO);  // 0: 0
     ToN(0, o++);  // 0: o
-    ApplyAtZero(1);  // 1: attack(0)(o)
-    _(PUT, 0);  // 0: I
-    _(0, ZERO);  // 0: 0
-    ToN(0, 2);  // 0: 2
-    _(GET, 0);  // 0: 11112
-    ApplyAtZero(1);  // 1: attack(0)(o)(11112) -> I
+    CallWithSlot(1, 0);  // 1: attack(0)(o)
+    CallWithSlot(1, 2);  // 1: attack(0)(o)(11112) -> I
     _(PUT, 0);  // 0: I
   }
 
@@ -137,12 +150,8 @@ void DoWork() {
     _(ATTACK, 1);  // 1: attack(1)
     _(0, ZERO);  // 0: 0
     ToN(0, o++);  // 0: o
-    ApplyAtZero(1);  // 1: attack(1)(o)
-    _(PUT, 0);  // 0: I
-    _(0, ZERO);  // 0: 0
-    ToN(0, 2);  // 0: 2
-    _(GET, 0);  // 0: 11112
-    ApplyAtZero(1);  // 1: attack(1)(o)(11112) -> I
+    CallWithSlot(1, 0);  // 1: attack(1)(o)
+    CallWithSlot(1, 2);  // 1: attack(1)(o)(11112) -> I
     _(PUT, 0);  // 0: I
   }
 }
@@ -151,28 +160,20 @@ void Work() {
   // Precompute functions/values.
 
   // 2: 11112
-  _(2, ZERO);  // 2: 0
-  ToN(2, 11112);  // 2: 11112
+  IToN(2, 11112);  // 2: 11112
 
   // 3: 8192
-  _(3, ZERO);  // 3: 0
-  ToN(3, 8192);  // 3: 8192
+  IToN(3, 8192);  // 3: 8192
 
   // 4: help(0)(1)
-  _(0, ZERO);  // 0: 0
-  _(SUCC, 0);  // 0: 1
   _(4, HELP);  // 4: help
-  _(4, ZERO);  // 4: help(0)
-  ApplyAtZero(4);  // 4: help(0)(1)
-  _(PUT, 0);  // 0: I
+  CallWithValue(4, 0);  // 4: help(0)
+  CallWithValue(4, 1);  // 4: help(0)(1)
 
   // 5: help(1)(0)
-  _(0, ZERO);  // 0: 0
-  _(5, ZERO);  // 5: 0
-  _(SUCC, 5);  // 5: 1
-  _(HELP, 5);  // 5: help(1)
-  ApplyAtZero(5);  // 5: help(1)(0)
-  _(PUT, 0);  // 0: I
+  _(5, HELP);  // 5: help
+  CallWithValue(5, 1);  // 5: help(1)
+  CallWithValue(5, 0);  // 5: help(1)(0)
 
   while (1) {
     DoWork();
