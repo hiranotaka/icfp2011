a5daac650f2fbef512c8bee649f41ab712a65aa1
hiranotaka <hiranotaka@cd1ffd91-e609-42d8-b181-33580f4ca86f>
2011-06-19 21:25:02 +0000

    Clean up parser_cli.c
    Signed-off-by: HIRANO Takahito <hiranotaka@zng.info>
    
    git-svn-id: file:///srv/subversion/icfp2011@74 cd1ffd91-e609-42d8-b181-33580f4ca86f

--- compiler/parser_cli.c
+++ compiler/parser_cli.c
@@ -1,15 +1,16 @@
-#include <stddef.h>
+#include <stdio.h>
 #include "parser.h"
 #include "../sim/types.h"
 #include "../sim/debug.h"
 
 int main(int argc, char *argv[]) {
+	struct value *value;
 	if (argc != 2)
 		return 1;
 
-	struct value *value;
 	if (!parse(argv[1], NULL, &value))
 		return 1;
+
 	print_value(value);
 	printf("\n");
 	unref_value(value);
